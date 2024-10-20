6fe39beb3d105bc32f50d5d289a6dc666b93a7e0
Rivera-Angel <rivera.angel2808@gmail.com>
2024-10-19 18:07:11 -0700

    Added Jump to Script
    Added Jump to Script, not functional yet

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,12 +6,17 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody2D body;
     PlayerInput playerInput;
     public float moveSpeed = 5f;
-
+    public float jumpForce = 7f;
+    public Transform groundCheck;
+    public LayerMask groundLayer;
     public PlayerInputActions playerControls;
 
     Vector2 moveDirection = Vector2.zero;
     private InputAction move;
     private InputAction fire;
+    private InputAction jump;
+
+    private bool isGrounded;
 
     private void Start()
     {
@@ -32,12 +37,16 @@ public class PlayerMovement : MonoBehaviour
         fire.Enable();
         fire.performed += Fire;
 
+        jump = playerControls.Player.Jump;
+        jump.Enable();
+        jump.performed += jump;
     }
 
     private void OnDisable()
     {
         move.Disable();
         fire.Disable();
+        jump.Disable();
     }
 
     private void Update()
@@ -46,12 +55,21 @@ public class PlayerMovement : MonoBehaviour
         //moveDirection = new Vector2(moveX, 0).normalized;
 
         moveDirection = move.ReadValue<Vector2>();
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer); // Ground check
 
     }
 
     private void FixedUpdate()
     {
         body.linearVelocityX = moveDirection.x * moveSpeed;
+    }
+
+    private void Jump(InputAction.CallbackContext context)
+    {
+        if (isGrounded)
+        {
+            body.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse); // Apply jump force
+        }
     }
 
     private void Fire(InputAction.CallbackContext context)
